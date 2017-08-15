7f077d5b78bbe03417cd7593cd7c4830d75be096
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-08-15 09:43:01 +0100

    Update AuthServiceProvider.php

--- src/Auth/AuthServiceProvider.php
+++ src/Auth/AuthServiceProvider.php
@@ -10,25 +10,36 @@
 	use Illuminate\Auth\Guard;
 	
 	class AuthServiceProvider extends BaseAuthServiceProvider {
-	
-	    public function registerAuthenticator()
-	    {
-		    
-		    Route::aliasMiddleware('auth.basic', BasicAuth::class);
-		    Route::aliasMiddleware('auth.form', FormAuth::class);
-		    Route::aliasMiddleware('auth.token', TokenAuth::class);
-		    
-		    $this->app['auth']->extend('wordpress', function($app) {
-			    
-				$model = $app['config.factory']->get('auth.model');
-	
-				$provider = new UserProvider($app['hash'], $model);
-	
-				return new Guard($provider, $app['config.factory']->get('session.store'));
-				
-			});
-	        
-	    }
+		
+		/**
+		* Boot the service provider
+		*
+		* @return void
+		*/
+		public function boot() {
 
-	
+			$this->publishes([
+				__DIR__.'/../../config/auth.config.php' => config_path('auth.config.php')
+			], 'config');
+
+		}
+
+		public function registerAuthenticator() {
+
+			Route::aliasMiddleware('auth.basic', BasicAuth::class);
+			Route::aliasMiddleware('auth.form', FormAuth::class);
+			Route::aliasMiddleware('auth.token', TokenAuth::class);
+
+			$this->app['auth']->extend('wordpress', function($app) {
+
+				$model = $app['config.factory']->get('auth.model');
+
+				$provider = new UserProvider($app['hash'], $model);
+
+				return new Guard($provider, $app['config.factory']->get('session.store'));
+
+			});
+
+		}
+
 	}
