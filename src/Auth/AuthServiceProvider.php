a6010cb48096d44f5764b80b1e5ac9ba0a79e318
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-17 12:22:46 +0100

    Update AuthServiceProvider.php

--- src/Auth/AuthServiceProvider.php
+++ src/Auth/AuthServiceProvider.php
@@ -5,7 +5,7 @@
 	use Illuminate\Support\ServiceProvider;
 	use Illuminate\Contracts\Auth\Factory;
 	use Themosis\Facades\Route;
-	use WPKit\Auth\Middleware\FormAuth;
+	use WPKit\Auth\Middleware\WpLoginAuth;
 	use WPKit\Auth\Middleware\TokenAuth;
 	use WPKit\Auth\Middleware\BasicAuth;
 	
@@ -19,7 +19,7 @@
 		public function register() {
 
 			Route::aliasMiddleware('auth.basic', BasicAuth::class);
-			Route::aliasMiddleware('auth.form', FormAuth::class);
+			Route::aliasMiddleware('auth.wp_login', WpLoginAuth::class);
 			Route::aliasMiddleware('auth.token', TokenAuth::class);
 			
 			$this->app->auth->provider('wordpress', function() {
