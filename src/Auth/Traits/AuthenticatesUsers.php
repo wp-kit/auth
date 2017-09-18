552e1793a07c857be0b99b917289217a0ca240bc
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-18 20:17:01 +0100

    Update AuthenticatesUsers.php

--- src/Auth/Traits/AuthenticatesUsers.php
+++ src/Auth/Traits/AuthenticatesUsers.php
@@ -13,11 +13,12 @@ trait AuthenticatesUsers
     /**
      * Show the application's login form.
      *
+     * @param array $errors
      * @return \Illuminate\Http\Response
      */
-    public function showLoginForm()
+    public function showLoginForm( $errors = array() )
     {
-        return view('auth.login');
+        return view('auth.login', compact('errors'));
     }
 
     /**
@@ -28,27 +29,36 @@ trait AuthenticatesUsers
      */
     public function login(Request $request)
     {
-        $this->validateLogin($request);
-
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if ($this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
-
-            return $this->sendLockoutResponse($request);
-        }
-
-        if ($this->attemptLogin($request)) {
-            return $this->sendLoginResponse($request);
-        }
-
-        // If the login attempt was unsuccessful we will increment the number of attempts
-        // to login and redirect the user back to the login form. Of course, when this
-        // user surpasses their maximum number of attempts they will get locked out.
-        $this->incrementLoginAttempts($request);
-
-        return $this->sendFailedLoginResponse($request);
+	    
+	    try {
+		    
+	        $this->validateLogin($request);
+	
+	        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+	        // the login attempts for this application. We'll key this by the username and
+	        // the IP address of the client making these requests into this application.
+	        if ($this->hasTooManyLoginAttempts($request)) {
+	            $this->fireLockoutEvent($request);
+	
+	            return $this->sendLockoutResponse($request);
+	        }
+	
+	        if ($this->attemptLogin($request)) {
+	            return $this->sendLoginResponse($request);
+	        }
+	
+	        // If the login attempt was unsuccessful we will increment the number of attempts
+	        // to login and redirect the user back to the login form. Of course, when this
+	        // user surpasses their maximum number of attempts they will get locked out.
+	        $this->incrementLoginAttempts($request);
+	
+	        return $this->sendFailedLoginResponse($request);
+	        
+		} catch(ValidationException $e) {
+			
+			return $this->showLoginForm( $e->errors() );
+			
+		}
     }
 
     /**
