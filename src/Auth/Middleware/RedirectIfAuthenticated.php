d58ce983975d35a456f4249b91e713f4155ae972
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-18 16:56:35 +0100

    Update RedirectIfAuthenticated.php

--- src/Auth/Middleware/RedirectIfAuthenticated.php
+++ src/Auth/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect(app('config.factory')->get('auth.guest.redirect'););
         }
 
         return $next($request);
