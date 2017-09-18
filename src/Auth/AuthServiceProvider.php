4c51d57d708e3312943739653be62f9299698063
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-18 19:38:56 +0100

    Update AuthServiceProvider.php

--- src/Auth/AuthServiceProvider.php
+++ src/Auth/AuthServiceProvider.php
@@ -4,6 +4,8 @@
 
 	use Illuminate\Support\ServiceProvider;
 	use Illuminate\Support\Facades\Facade;
+	use Illuminate\Routing\Redirector;
+	use Illuminate\Routing\UrlGenerator;
 	use Themosis\Facades\Route;
 	
 	class AuthServiceProvider extends ServiceProvider {
@@ -102,7 +104,72 @@
 				return $this->app->session;
 				
 			});
+			
+			$this->registerRedirector();
+			$this->registerUrlGenerator();
 
 		}
+		
+		/**
+	     * Register the URL generator service.
+	     *
+	     * @return void
+	     */
+	    protected function registerUrlGenerator()
+	    {
+	        $this->app->singleton('url', function ($app) {
+	            $routes = $app['router']->getRoutes();
+	            // The URL generator needs the route collection that exists on the router.
+	            // Keep in mind this is an object, so we're passing by references here
+	            // and all the registered routes will be available to the generator.
+	            $app->instance('routes', $routes);
+	            $url = new UrlGenerator(
+	                $routes, $app->rebinding(
+	                    'request', $this->requestRebinder()
+	                )
+	            );
+	            $url->setSessionResolver(function () {
+	                return $this->app['session'];
+	            });
+	            // If the route collection is "rebound", for example, when the routes stay
+	            // cached for the application, we will need to rebind the routes on the
+	            // URL generator instance so it has the latest version of the routes.
+	            $app->rebinding('routes', function ($app, $routes) {
+	                $app['url']->setRoutes($routes);
+	            });
+	            return $url;
+	        });
+	    }
+		
+		/**
+	     * Register the Redirector service.
+	     *
+	     * @return void
+	     */
+	    protected function registerRedirector()
+	    {
+	        $this->app->singleton('redirect', function ($app) {
+	            $redirector = new Redirector($app['url']);
+	            // If the session is set on the application instance, we'll inject it into
+	            // the redirector instance. This allows the redirect responses to allow
+	            // for the quite convenient "with" methods that flash to the session.
+	            if (isset($app['session.store'])) {
+	                $redirector->setSession($app['session.store']);
+	            }
+	            return $redirector;
+	        });
+	    }
+	    
+	    /**
+	     * Get the URL generator request rebinder.
+	     *
+	     * @return \Closure
+	     */
+	    protected function requestRebinder()
+	    {
+	        return function ($app, $request) {
+	            $app['url']->setRequest($request);
+	        };
+	    }
 
 	}
