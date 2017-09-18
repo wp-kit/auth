b0532087af0b8103103e42b17d49fc6bd041a79d
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-18 19:15:38 +0100

    Create helpers.php

---
+++ src/helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+ if (! function_exists('redirect')) {
+    /**
+     * Get an instance of the redirector.
+     *
+     * @param  string|null  $to
+     * @param  int     $status
+     * @param  array   $headers
+     * @param  bool    $secure
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    function redirect($to = null, $status = 302, $headers = [], $secure = null)
+    {
+        if (is_null($to)) {
+            return app('redirect');
+        }
+        return app('redirect')->to($to, $status, $headers, $secure);
+    }
+}
