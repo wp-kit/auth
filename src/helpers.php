bdf49544c0ff516e63c025627b73d504a463adb7
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-18 20:18:13 +0100

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -18,3 +18,21 @@
         return app('redirect')->to($to, $status, $headers, $secure);
     }
 }
+
+if (! function_exists('trans')) {
+    /**
+     * Translate the given message.
+     *
+     * @param  string  $key
+     * @param  array   $replace
+     * @param  string  $locale
+     * @return \Illuminate\Contracts\Translation\Translator|string|array|null
+     */
+    function trans($key = null, $replace = [], $locale = null)
+    {
+        if (is_null($key)) {
+            return app('translator');
+        }
+        return app('translator')->trans($key, $replace, $locale);
+    }
+}
