73090fb0ee566c3df2a674466741b29f8419270a
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-18 17:33:49 +0100

    Create app.config.php

---
+++ config/app.config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Locale Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The application locale determines the default locale that will be used
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
+    |
+    */
+
+    'locale' => 'en'
+
+];
